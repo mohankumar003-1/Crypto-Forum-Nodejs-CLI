93569429530bd90c6e5d4c8ae5cff3f6afefc2ed
mohankumar003-1 <smohankumar018@gmail.com>
2024-03-22 17:27:26 +0530

    Intial SetUps

---
+++ commands/key.js
@@ -0,0 +1,15 @@
+const key = {
+    set(){
+        console.log("Set");
+    },
+    show(){
+        console.log("Show");
+    },
+    remove(){
+        console.log("remove");
+    },
+
+
+
+}
+module.exports = key;
